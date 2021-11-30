77e1cb6b75e5dcb484f465087fb15024bc74ad42
nikalex5555 <nikalex5555@gmail.com>
2021-11-30 22:03:26 +0300

    Area of a Circle 7kyu

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function perimeterSequence(a,n) {
-  return a*n*4
-}
-console.log(perimeterSequence(1,3)) 
+var circleArea = function (radius) {
+  if (radius <= 0 || isNaN(radius) || "string" === typeof (radius)) {
+    return false;
+  } else {
+   let res= (Math.PI * Math.pow(radius, 2)).toFixed(2);
+   return Number(res)
+  }
+};
+console.log(circleArea(-1485.86)); //returns false
+console.log(circleArea(0)); //returns false
+console.log(circleArea(43.26,73)); //returns 5881.25
+console.log(circleArea(68)); //returns 14526.72
+console.log(circleArea(NaN));
