9e1e6a16385ed6e4dddcf876c7f76431acc55192
nikalex5555 <nikalex5555@gmail.com>
2021-12-02 23:14:38 +0300

    101 Dalmatians - squash the bugs, not the dogs!

--- script.js
+++ script.js
@@ -34,6 +34,44 @@ function bump(x) {
   }
 }
 
-console.log(bump("n")); // "Woohoo!");
-console.log(bump("_nnnnnnn_n__n______nn__nn_nnn")); // "Car Dead");
-console.log(bump("______n___n_")); // "Woohoo!");
+// console.log(bump("n")); // "Woohoo!");
+// console.log(bump("_nnnnnnn_n__n______nn__nn_nnn")); // "Car Dead");
+// console.log(bump("______n___n_")); // "Woohoo!");
+
+/* function last(x) {
+  let newAr = x.split(" ");
+  newAr.sort(function (a, b) {
+    return newAr[][]
+  }); */
+
+  // let arr=[];
+  // for(let i=newAr.length-1; i>=0; i--){
+  //   arr.push(newAr[i])
+  // }
+  //console.log(arr)
+
+  //const upRev=rev.reverse();
+  // var arr=[];
+  // for(let i=x.length; i<=0; i--){
+  //   arr.push(upRev[i])
+  // }
+//}
+//console.log(last("man i need a taxi up to ubud")); //, ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+// console.log(last('what time are we climbing up the volcano'))//, ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']);
+// console.log(last('take me to semynak'))//, ['take', 'me', 'semynak', 'to']);
+
+  var dogs = [
+    "Hardly any",
+    "More than a handful!",
+    "Woah that's a lot of dogs!",
+    "101 DALMATIONS!!!",
+  ];
+const howManyDalmatians = number=>  number <= 10 ? dogs[0] : (number <= 50 ? dogs[1]: (number === 101 ? dogs[3] : dogs[2]));
+
+
+console.log(howManyDalmatians(26)); //, "More than a handful!");
+console.log(howManyDalmatians(8)); //, "Hardly any");
+console.log(howManyDalmatians(14)); //, "More than a handful!");
+console.log(howManyDalmatians(80)); //, "Woah that's a lot of dogs!");
+console.log(howManyDalmatians(100)); //, "Woah that's a lot of dogs!");
+console.log(howManyDalmatians(101)); //, "101 DALMATIANS!!!");
