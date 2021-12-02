b40f4d0a83c3f9de6ec7804c61b433764918e1ca
nikalex5555 <nikalex5555@gmail.com>
2021-12-02 23:43:45 +0300

    Squash the bugs

--- script.js
+++ script.js
@@ -44,34 +44,53 @@ function bump(x) {
     return newAr[][]
   }); */
 
-  // let arr=[];
-  // for(let i=newAr.length-1; i>=0; i--){
-  //   arr.push(newAr[i])
-  // }
-  //console.log(arr)
+// let arr=[];
+// for(let i=newAr.length-1; i>=0; i--){
+//   arr.push(newAr[i])
+// }
+//console.log(arr)
 
-  //const upRev=rev.reverse();
-  // var arr=[];
-  // for(let i=x.length; i<=0; i--){
-  //   arr.push(upRev[i])
-  // }
+//const upRev=rev.reverse();
+// var arr=[];
+// for(let i=x.length; i<=0; i--){
+//   arr.push(upRev[i])
+// }
 //}
 //console.log(last("man i need a taxi up to ubud")); //, ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
 // console.log(last('what time are we climbing up the volcano'))//, ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']);
 // console.log(last('take me to semynak'))//, ['take', 'me', 'semynak', 'to']);
 
-  var dogs = [
-    "Hardly any",
-    "More than a handful!",
-    "Woah that's a lot of dogs!",
-    "101 DALMATIONS!!!",
-  ];
-const howManyDalmatians = number=>  number <= 10 ? dogs[0] : (number <= 50 ? dogs[1]: (number === 101 ? dogs[3] : dogs[2]));
+var dogs = [
+  "Hardly any",
+  "More than a handful!",
+  "Woah that's a lot of dogs!",
+  "101 DALMATIONS!!!",
+];
+const howManyDalmatians = (number) =>
+  number <= 10
+    ? dogs[0]
+    : number <= 50
+    ? dogs[1]
+    : number === 101
+    ? dogs[3]
+    : dogs[2];
 
+// console.log(howManyDalmatians(26)); //, "More than a handful!");
+// console.log(howManyDalmatians(8)); //, "Hardly any");
+// console.log(howManyDalmatians(14)); //, "More than a handful!");
+// console.log(howManyDalmatians(80)); //, "Woah that's a lot of dogs!");
+// console.log(howManyDalmatians(100)); //, "Woah that's a lot of dogs!");
+// console.log(howManyDalmatians(101)); //, "101 DALMATIANS!!!");
 
-console.log(howManyDalmatians(26)); //, "More than a handful!");
-console.log(howManyDalmatians(8)); //, "Hardly any");
-console.log(howManyDalmatians(14)); //, "More than a handful!");
-console.log(howManyDalmatians(80)); //, "Woah that's a lot of dogs!");
-console.log(howManyDalmatians(100)); //, "Woah that's a lot of dogs!");
-console.log(howManyDalmatians(101)); //, "101 DALMATIANS!!!");
+function findLongest(str) {
+  let spl = str.split(" ");
+  let res = spl.sort(function (a, b) {
+    return b.length - a.length;
+  });
+  return res[0].length;
+}
+console.log(findLongest("The quick white fox jumped around the massive dog")); //, 7);
+console.log(findLongest("Take me to tinseltown with you")); //, 10);
+console.log(findLongest("Sausage chops")); //, 7);
+console.log(findLongest("Wind your body and wiggle your belly")); //, 6);
+console.log(findLongest("Lets all go on holiday")); //, 7);
