7126055784d136bd00e8757b24bf8c12825f3b96
nikalex5555 <nikalex5555@gmail.com>
2021-12-05 01:36:43 +0300

    What's the real floor?

--- script.js
+++ script.js
@@ -337,3 +337,9 @@ function DNAtoRNA(dna) {
 
       return str.split('').reverse().join('')
      }
+
+
+     //What's the real floor?
+     function getRealFloor(n) {
+      return n<0?n:n==1||n==0?0:n<=13?n-1:n>13?n-2:0
+   }
