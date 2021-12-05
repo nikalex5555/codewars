1c0d2083dc137390015c8d1b6604382404073152
nikalex5555 <nikalex5555@gmail.com>
2021-12-05 18:05:17 +0300

    getNames()

--- script.js
+++ script.js
@@ -555,3 +555,8 @@ function evenNumbers(array, number) {
   console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))//, [4, 6, 8]);
   console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))//, [-8, 26]);
   console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1))//, [6]);
+
+//getNames()
+  function getNames(data){
+    return data.map(function(item){return item.name});
+  }
