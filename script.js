3ea659f5745261b255d8981185e1f8a427a74316
nikalex5555 <nikalex5555@gmail.com>
2021-12-05 04:49:18 +0300

    Training JS #11: loop statement -break,continue

--- script.js
+++ script.js
@@ -377,3 +377,44 @@ console.log(pickIt([3, 2, 1])); //,[[3,1],[2]]);
 console.log(pickIt([10, 20, 30])); //,[[],[10,20,30]]);
 console.log(pickIt([11, 21, 31])); //,[[11,21,31],[]]);
 console.log(pickIt([8, 1, 5, 4, 6, 1, 1])); //,[[1,5,1,1],[8,4,6]]);
+
+//Training JS #11: loop statement --break,continue
+function grabDoll(dolls) {
+  var bag = [];
+  for (let i = 0; i < dolls.length; i++) {
+    // console.log(bag)
+    if (dolls[i] == "Hello Kitty" || dolls[i] == "Barbie doll") {
+      bag.push(dolls[i]);
+    }
+    if (dolls[i] == "Mickey Mouse") {
+      continue;
+    }
+    if (bag.length == 3) {
+      break;
+    }
+  }
+  return bag;
+}
+console.log(grabDoll(["Mickey Mouse", "Hello Kitty", "Snow white"])); //,["Hello Kitty"]);
+console.log(
+  grabDoll(["Mickey Mouse", "Hello Kitty", "Hello Kitty", "Snow white"])
+); //,["Hello Kitty","Hello Kitty"]);
+console.log(
+  grabDoll([
+    "Mickey Mouse",
+    "Hello Kitty",
+    "Hello Kitty",
+    "Barbie doll",
+    "Snow white",
+  ])
+); //,["Hello Kitty","Hello Kitty","Barbie doll"]);
+console.log(
+  grabDoll([
+    "Mickey Mouse",
+    "Barbie doll",
+    "Hello Kitty",
+    "Hello Kitty",
+    "Hello Kitty",
+    "Snow white",
+  ])
+); //,["Barbie doll","Hello Kitty","Hello Kitty"]);
