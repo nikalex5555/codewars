c55d95103f048befd2205569924672ccac064648
nikalex5555 <nikalex5555@gmail.com>
2021-12-06 01:23:02 +0300

    Keep up the hoop

--- script.js
+++ script.js
@@ -753,3 +753,10 @@ function between(a, b) {
   return arr;
 }
 console.log(between(1, 4));
+
+//Keep up the hoop
+function hoopCount (n) {
+return   n<10?"Keep at it until you get it" :"Great, now move on to tricks" ;
+}
+console.log(hoopCount(3))//,"Keep at it until you get it" ) 
+console.log(hoopCount(11))//,"Great, now move on to tricks"  )
